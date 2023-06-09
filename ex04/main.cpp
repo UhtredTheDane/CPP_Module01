756270c13e84ed00afea24e6294871ababbe9156
agengemb <32024540+UhtredTheDane@users.noreply.github.com>
2023-06-09 21:28:32 +0200

    remise au propre

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -2,21 +2,35 @@
 #include <string>
 #include <fstream>
 
-int main(int argc, char **argv)
+void	find_and_replace(char **argv, std::ifstream& in_file, std::ofstream& out_file)
 {
 	std::string line;
-	std::string	new_name;
 	std::string s1;
 	std::string s2;
-	std::ifstream    in_file;
-	std::ofstream    out_file;
 	size_t  pos;
 
-	if (argc != 4)
+	s1 = argv[2];
+	s2 = argv[3];
+	while (getline(in_file, line))
 	{
-		std::cout << "Usage: ./sed nameFile s1 s2" << std::endl;
-		return (1);
+		pos = 0;
+		while ((pos = line.find(s1, pos)) != line.npos)
+		{
+			line.erase(pos, s1.length());
+			line.insert(pos, s2);
+			pos += s2.length();
+		}
+			out_file << line << std::endl;
 	}
+	out_file.close();
+}
+
+void	ft_sed(char **argv)
+{
+	std::string	new_name;
+	std::ifstream    in_file;
+	std::ofstream    out_file;
+
 	in_file.open(argv[1]);
 	if (in_file)
 	{
@@ -24,23 +38,18 @@ int main(int argc, char **argv)
 		new_name.append(".replace");
 		out_file.open(new_name.c_str());
 		if (out_file)
-		{
-			s1 = argv[2];
-			s2 = argv[3];
-			while (getline(in_file, line))
-			{
-				pos = 0;
-				while ((pos = line.find(s1, pos)) != line.npos)
-				{
-					line.erase(pos, s1.length());
-					line.insert(pos, s2);
-					pos += s2.length();
-				}
-				out_file << line << std::endl;
-			}
-			out_file.close();
-		}
+			find_and_replace(in_file, out_file);
 		in_file.close();
 	}
+}
+
+int main(int argc, char **argv)
+{
+	if (argc != 4)
+	{
+		std::cout << "Usage: ./sed nameFile s1 s2" << std::endl;
+		return (1);
+	}
+	ft_sed(argv);
 	return (0);
 }
--- ex06/main.cpp
+++ ex06/main.cpp
@@ -1,39 +1,54 @@
 #include "Harl.hpp"
 
-int main(int argc, char **argv)
+int	select_choice(std::string& choice)
+{
+	std::string levels[] = {"debug", "info" ,"warning" ,"error"};
+
+	for (int i = 0; i < 4; ++i)
+	{
+		if (choice == levels[i])
+			return (i);
+	}
+	return (-1);
+}
+
+void	filter_menu(std::string choice)
 {
 	Harl    harl;
 	int	num_complain;
-	std::string levels[] = {"debug", "info" ,"warning" ,"error"};
 
+	num_complain = select_choice(choice);
+	switch (num_complain)
+	{
+		case 0:	std::cout << "[ DEBUG ]" << std::endl;	
+			harl.complain("debug");
+			harl.complain("info");
+			harl.complain("warning");
+			harl.complain("error");
+			break;
+		case 1: std::cout << "[ INFO ]" << std::endl;
+			harl.complain("info");
+			harl.complain("warning");
+			harl.complain("error");
+			break;
+		case 2:	std::cout << "[ WARNING ]" << std::endl; 
+			harl.complain("warning");
+			harl.complain("error");
+			break;
+		case 3:	std::cout << "[ ERROR ]" << std::endl;
+			harl.complain("error");
+			break;
+		default:std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
+	}
+}
+
+int main(int argc, char **argv)
+{
 	if (argc != 2)
 	{
 		std::cout << "Usage: ./HarlFilter level" << std::endl;
 		return (1); 
 	}
-	num_complain = -1;
-	for (int i = 0; i < 4; ++i)
-	{
-		if (argv[1] == levels[i])
-			num_complain = i;
-	}
-	switch (num_complain)
-	{
-		case 0:	harl.complain("debug");
-			harl.complain("info");
-			harl.complain("warning");
-			harl.complain("error");
-			break;
-		case 1: harl.complain("info");
-			harl.complain("warning");
-			harl.complain("error");
-			break;
-		case 2: harl.complain("warning");
-			harl.complain("error");
-			break;
-		case 3: harl.complain("error");
-			break;
-		default:std::cout << "Euh je sais pas quoi dire" << std::endl;
-	}
+	filter_menu(argv[1]);
 	return (0);
 }
